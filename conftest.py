4d0c90a926c44a16816e97208fb93e8cfeb11625
amaym0n <loco.skitls@gmail.com>
2021-12-21 18:10:21 +0300

    Added conditions for the paths and some logic for the logger class

--- conftest.py
+++ conftest.py
@@ -8,5 +8,25 @@ from src.logger import Logger
 
 
 @pytest.fixture(scope='session', autouse=True)
-def main_fixture():
+def main_fixture(request):
 
+    LOGGER = Logger(request.get_atr('--logfile'))
+    LOGGER.report_info("Logger was created")
+
+    try:
+
+        if not exists(conf.logs):
+            mkdir(conf.logs)
+        if not exists(conf.inputs):
+            mkdir(conf.inputs)
+        if not exists(conf.outputs):
+            mkdir(conf.outputs)
+
+    except Exception as exc:
+        LOGGER.report_error(f"{exc}")
+        raise
+
+
+@pytest.fixture(scope='session', autouse=True)
+def params_parser(request):
+    ...
--- src/logger.py
+++ src/logger.py
@@ -3,15 +3,16 @@ import logging
 
 
 class Logger:
-    def __init__(self):
+    def __init__(self, logfile_name: str):
+        with open(f"{conf.logs}/{logfile_name}", 'w') as logger:
+            self.logger = logger
+
+    def report_warning(self, message: str):
         ...
 
-    def report_warning(self):
+    def report_error(self, message: str):
         ...
 
-    def report_error(self):
-        ...
-
-    def report_info(self):
+    def report_info(self, message: str):
         ...
 
