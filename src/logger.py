6f4f44d54e720554a7298093f2eba49222082094
amaym0n <loco.skitls@gmail.com>
2021-12-21 17:53:29 +0300

    Added new methods to the logger class

--- src/logger.py
+++ src/logger.py
@@ -1,1 +1,16 @@
 import logging
+
+
+class Logger:
+    def __init__(self):
+        ...
+
+    def report_warning(self):
+        ...
+
+    def report_error(self):
+        ...
+
+    def report_info(self):
+        ...
+
