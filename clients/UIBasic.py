0462c3e04484bf0e2785053a6bb76556227d61e2
amaym0n <loco.skitls@gmail.com>
2022-10-30 23:45:18 +0300

    added docstrings to methods in UIBasic.py

--- clients/UIBasic.py
+++ clients/UIBasic.py
@@ -17,42 +17,49 @@ class UIBasic:
 
     def element_presence(self, element: tuple[By, str], waiting_time: int = 10,
                          driver: Optional[WebDriver] = None) -> WebElement:
+        """ Проверить, что элемент представлен на странице """
         with allure.step(f'Проверить, что элемент представлен на странице - \n{element=}'):
             return WebDriverWait(driver=self.driver if not driver else driver, timeout=waiting_time) \
                 .until(expected_conditions.presence_of_element_located(locator=element))
 
     def all_elements_presence(self, element: tuple[By, str], waiting_time: int = 10,
                               driver: Optional[WebDriver] = None) -> list[WebElement]:
+        """ Проверить, что все элементы представлены на странице """
         with allure.step(f'Проверить что все элементы представлены на странице - \n{element=}'):
             return WebDriverWait(driver=self.driver if not driver else driver, timeout=waiting_time) \
                 .until(expected_conditions.presence_of_all_elements_located(locator=element))
 
     def element_clickable(self, element: Union[tuple[By, str], WebElement],
                           waiting_time: int = 10, driver: Optional[WebDriver] = None) -> Union[WebElement, bool]:
+        """ Проверить что элемент на странице кликабелен """
         with allure.step(f'Проверить что элемент на странице кликабелен - \n{element=}'):
             return WebDriverWait(driver=self.driver if not driver else driver, timeout=waiting_time) \
                 .until(expected_conditions.element_to_be_clickable(mark=element))
 
     def element_missing(self, element: Union[tuple[By, str], WebElement],
                         waiting_time: int = 10, driver: Optional[WebDriver] = None) -> Union[WebElement, bool]:
+        """ Проверить что элемент пропал со страницы """
         with allure.step(f'Проверить что элемент пропал со страницы - \n{element=}'):
             return WebDriverWait(driver=self.driver if not driver else driver, timeout=waiting_time) \
                 .until(expected_conditions.invisibility_of_element(element=element))
 
     def text_in_element(self, element: tuple[By, str], text: str, waiting_time: int = 10,
                         driver: Optional[WebDriver] = None) -> bool:
+        """ Проверить что текст внутри элемента совпадает с ожидаемым """
         with allure.step(f'Проверить что текст внутри элемента совпадает с ожидаемым - \n{element=}'):
             return WebDriverWait(driver=self.driver if not driver else driver, timeout=waiting_time) \
                 .until(expected_conditions.text_to_be_present_in_element(locator=element, text_=text))
 
     def text_in_element_attribute(self, element: tuple[By, str], text: str, attribute: str,
                                   waiting_time: int = 10, driver: Optional[WebDriver] = None) -> bool:
+        """ Проверить что текст внутри атрибута элемента совпадает с ожидаемым """
         with allure.step(f'Проверить что текст внутри атрибута элемента совпадает с ожидаемым - \n{element=}'):
             return WebDriverWait(driver=self.driver if not driver else driver, timeout=waiting_time) \
                 .until(expected_conditions.text_to_be_present_in_element_attribute(locator=element, text_=text,
                                                                                    attribute_=attribute))
 
     def alert_window_presence(self, waiting_time: int = 10) -> Union[Alert, bool]:
-        with allure.step(f'Проверить что сигнальное окно представлено на странице'):
+        """ Проверить что сигнальное окно представлено на странице """
+        with allure.step('Проверить что сигнальное окно представлено на странице'):
             return WebDriverWait(driver=self.driver, timeout=waiting_time) \
                 .until(expected_conditions.alert_is_present())
