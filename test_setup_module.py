f560150cddc00e20a8e71abe8345019684262323
amaym0n <loco.skitls@gmail.com>
2023-04-21 21:02:49 +0300

    Updated clients and did some fixes in code

--- test_setup_module.py
+++ test_setup_module.py
@@ -1,17 +1,19 @@
 import allure
 import pytest
 
-from clients.DBClient import DBClient
+from clients.db_client import DBClient
 from conftest import create_path_to_file
 
 
 @pytest.mark.test_data
-@pytest.mark.parametrize('file_name', ['sql_query.sql'])
+@pytest.mark.parametrize("file_name", ["sql_query.sql"])
 def test_insert_data_to_db(conf, file_name):
     file_root = create_path_to_file(file_name=file_name)
-    with open(file=file_root, mode='rb') as sql_queries:
-        queries = [query.strip('/r/n') for query in sql_queries.read().decode().split(';')]
-    with allure.step('Создание необходимых записей в БД'):
-        with DBClient(connection_string=conf['connection_string']) as db:
+    with open(file=file_root, mode="rb") as sql_queries:
+        queries = [
+            query.strip("/r/n") for query in sql_queries.read().decode().split(";")
+        ]
+    with allure.step("Создание необходимых записей в БД"):
+        with DBClient(connection_string=conf["connection_string"]) as db:
             for query in queries:
                 db.cursor.execute(query=query)
