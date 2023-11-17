b38490e1af379a05e59497cdc3572712915a9594
amaym0n <loco.skitls@gmail.com>
2023-11-17 09:52:25 +0300

    Updated clients and dependencies

--- clients/api_client.py
+++ clients/api_client.py
@@ -1,62 +1,73 @@
 from __future__ import annotations
 
+import json
 from http import HTTPStatus
-from typing import Any
-from typing import Callable
+from typing import Callable, Any, Generator
 
 import allure
-import requests
-from requests import Response
-
-from helpers.models import StandConfig
-from clients.auth.jwt_auth import JWTAuth
-
-
-def timeout_logging(func: Callable[..., Any]) -> Callable[..., Any]:
-    """Decorator to add requests when request fails due to timeout"""
-
-    def logging(*args: object, **kwargs: dict[str, Any]) -> Response:
-        try:
-            return func(*args, **kwargs)
-        except requests.exceptions.Timeout as e:
-            with allure.step(f"[{e.request.method}] --> {e.request.url}"):
-                req_text = f"""[{e.request.method}] --> {e.request.url} \n'
-                           headers: {e.request.headers} \n body: {e.request.body if e.request.body else ""}"""
-                allure.attach(req_text, "request", allure.attachment_type.TEXT)
-                raise requests.exceptions.Timeout(e)
-
-    return logging
+from httpx import Client, Auth
+from httpx import Request
+from httpx import Response
+from pydantic import HttpUrl
 
 
-def allure_listener(response: Response, *args: Any, **kwargs: Any) -> None:
-    """ Function for logging requests and responses for ApiClient """
-
-    def formatter(resp: Response) -> tuple[str, str]:
-        """ Formatting information for logging """
-        req_data = (
-            f'[{resp.request.method}] --> {resp.url} \n' f' headers: {resp.headers}'
-        )
-        if resp.request.method == 'POST':
-            req_data += f'\n body: {resp.request.body}'
-        req_message = req_data
-        resp_message = (
-            f'status_code: {resp.status_code} \n {req_data} \n Content: \n {resp.text}'
-        )
-        return req_message, resp_message
-
-    with allure.step(f"[{response.request.method}] --> {response.url}"):
-        req_text, resp_text = formatter(response)
-        allure.attach(req_text, 'request', allure.attachment_type.TEXT)
-        allure.attach(resp_text, 'response', allure.attachment_type.TEXT)
+def request_hook(request: Request) -> None:
+    """  """
+    with allure.step(title=f'Request: [{request.method}] --> {request.url}'):
+        headers = request.headers
+        body = '' if request.content == b'' \
+            else f" --data '{request.content if isinstance(request.content, str) else request.content.decode()}'"
+        curl = f"curl --location '{request.url}' --header '{json.dumps(dict(headers))}'{body}"
+        print(curl)
+        allure.attach(curl, 'request', allure.attachment_type.TEXT)
+    return
 
 
-class ApiClient:
-    def __init__(self, config: StandConfig, auth: JWTAuth) -> None:
-        self.base_url = f'{config.protocol}{config.url}'
+def response_hook(response: Response) -> None:
+    """  """
+    with allure.step(title=f'Response: [{response.request.method}] --> {response.request.url}'):
+        response.read()
+        resp_message = f'status_code: {response.status_code} \n  Content: \n {response.text}'
+        print(resp_message)
+        allure.attach(resp_message, 'response', allure.attachment_type.TEXT)
+    return
+
+
+class JWTAuth(Auth):
+    def __init__(self, jwt_token: str) -> None:
+        self.jwt_token = jwt_token
+        if not self.jwt_token:
+            raise Exception("jwt_token is mandatory.")
+
+    def auth_flow(
+            self, request: Request
+    ) -> Generator[Request, Response, None]:
+        request.headers['Authorization'] = f'Bearer {self.jwt_token}'
+        yield request
+
+
+class APIClient(Client):
+
+    def __init__(
+            self,
+            base_url: HttpUrl,
+            auth: Callable[..., Any] | object | None = None,
+            verify: bool = False,
+    ) -> None:
+        """
+            Args:
+                base_url (HttpUrl): The base URL to be used for requests.
+                auth (Callable[..., Any] | object, optional): An authentication object or callable authentication object
+                    that will be used for request authorization. If not provided, no authentication
+                    will be used. Defaults to None.
+                verify (bool, optional): Determines whether to verify SSL certificates for HTTPS
+                    requests. Defaults to False.
+        """
+        super().__init__(auth=None, verify=verify, event_hooks={'request': [request_hook], 'response': [response_hook]})
         self.auth = auth
+        self.base_url = base_url
 
-    @timeout_logging
-    def request(
+    def send_request(
             self,
             method: str,
             path: str,
@@ -65,26 +76,26 @@ class ApiClient:
             data: dict | None = None,
             json: dict | None = None,
             files: dict | list | None = None,
-            allow_redirects: bool = True,
-            verify: bool = False,
+            follow_redirects: bool = True,
+            timeout=300,
             status_code: int = HTTPStatus.OK,
     ):
         """ Send HTTP-request """
-        response = requests.request(
+        response = self.request(
             method=method,
             url=f'{self.base_url}{path}',
             headers=headers, params=params,
             data=data, json=json,
-            files=files,
-            allow_redirects=allow_redirects,
-            verify=verify
+            files=files, auth=self.auth,
+            follow_redirects=follow_redirects,
+            timeout=timeout,
         )
         self.check_status_code(response=response, status_code=status_code)
         return response
 
     @staticmethod
-    def check_status_code(response: Response, status_code: int) -> None:
+    def check_status_code(response, status_code: int) -> None:
         with allure.step('Checking the status of the request code'):
             assert response.status_code == status_code, \
-                f"""Wrong status code, expected: {status_code}, received: {response.status_code}, 
-                message: {response.text}"""
+                f"""Wrong status code, expected: {status_code}, received: {response.status_code},
+                    message: {response.text}"""
--- clients/db_client.py
+++ clients/db_client.py
@@ -1,56 +1,106 @@
 from __future__ import annotations
 
-import functools
-from typing import Callable, Any
+from typing import Any
+from typing import Callable
 
 import allure
-import psycopg2
-from psycopg2.extensions import connection
-from psycopg2.extensions import cursor
+from psycopg import connect
 
 
-def allure_listener(func: Callable[..., Any]) -> Callable[..., Any]:
-    """ Decorator that attaches the SQL query as an allure attachment """
-
-    @functools.wraps(wrapped=func)
-    def wrapper(self: DBClient, query: str, *args: Any, **kwargs: Any) -> Any:
-        """ Wrapper function that attaches the SQL query and vars as an allure attachment """
-        result = func(self, query, *args, **kwargs)
-        with allure.step(title='SQL query and query result --->'):
-            allure.attach(query, name='SQL Query', attachment_type=allure.attachment_type.TEXT)
-            allure.attach(str(result), name='SQL Result', attachment_type=allure.attachment_type.TEXT)
+def db_query_listener(func: Callable[..., Any]) -> Callable[..., Any]:
+    def wrapper(*args, **kwargs) -> Any:
+        result = func(*args, **kwargs)
+        with allure.step(title='Query to DataBase: '):
+            allure.attach(kwargs['query'], name='Query to DataBase', attachment_type=allure.attachment_type.TEXT)
+        with allure.step(title='Result of query: '):
+            allure.attach(str(result), name='Query Result', attachment_type=allure.attachment_type.TEXT)
         return result
 
     return wrapper
 
 
 class DBClient:
-    """ Object that helps to easily work with db """
-
     def __init__(self, connection_string: str) -> None:
         self.connection_string = connection_string
-        self.connection: connection | None = None
-        self.cursor: cursor | None = None
 
-    @allure_listener
-    def get_first_row(self, query: str) -> ...:
-        """ Get first row from query result """
-        self.cursor.execute(query=query)
-        return self.cursor.fetchone()
+    @db_query_listener
+    def get_list(self, query: str, params: dict[str, Any] | None = None) -> list[Any]:
+        """
+            Args:
+                query (str): The SQL query to execute.
+                params (dict[str, Any] | None, optional): The parameters to substitute in the query.
+                    Defaults to None.
+        """
+        return [value[0] for value in self.select_all(query=query, params=params)]
 
-    @allure_listener
-    def select_all(self, query: str) -> list[tuple[...]]:
-        self.cursor.execute(query=query)
-        return self.cursor.fetchall()
+    @db_query_listener
+    def get_dict(self, query: str, params: dict[str, Any] | None = None) -> dict[Any, Any] | None:
+        """
+            Args:
+                query (str): The SQL query to execute.
+                params (dict[str, Any] | None, optional): The parameters to substitute in the query.
+                    Defaults to None.
+        """
+        result = self.select_all(query=query)
+        if not result and len(result) < 1:
+            return None
+        return {value[0]: value[1] for value in self.select_all(query=query, params=params)}
+
+    @db_query_listener
+    def select_all(self, query: str, params: dict[str, Any] | None = None) -> list[tuple]:
+        """
+            Args:
+                query (str): The SQL query to execute.
+                params (dict[str, Any] | None, optional): The parameters to substitute in the query.
+                    Defaults to None.
+        """
+        self.cursor.execute(query=query, params=params)
+        result = self.cursor.fetchall()
+        return result
+
+    @db_query_listener
+    def get_first_value(self, query: str, params: dict[str, Any] | None = None) -> Any:
+        """
+            Args:
+                query (str): The SQL query to execute.
+                params (dict[str, Any] | None, optional): The parameters to substitute in the query.
+                    Defaults to None.
+        """
+        result = self.get_first_row(query=query, params=params)
+        if not result:
+            return None
+        return result[0]
+
+    @db_query_listener
+    def get_first_row(self, query: str, params: dict[str, Any] | None = None) -> Any:
+        """
+            Args:
+                query (str): The SQL query to execute.
+                params (dict[str, Any] | None, optional): The parameters to substitute in the query.
+                    Defaults to None.
+        """
+        self.cursor.execute(query=query, params=params)
+        result = self.cursor.fetchone()
+        return result
+
+    @db_query_listener
+    def execute(self, query: str, params: dict[str, Any] | None = None) -> None:
+        """
+            Args:
+                query (str): The SQL query to execute.
+                params (dict[str, Any] | None, optional): The parameters to substitute in the query.
+                    Defaults to None.
+        """
+        self.cursor.execute(query=query, params=params)
+        self.connection.commit()
+        return
 
     def __enter__(self) -> DBClient:
-        """ Connect to database """
-        self.connection = psycopg2.connect(dsn=self.connection_string)
+        self.connection = connect(self.connection_string)
         self.cursor = self.connection.cursor()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-        """ Close database connection """
         self.cursor.close()
         self.connection.close()
         return
